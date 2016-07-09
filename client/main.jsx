674dd9ad4fdceac8812e17379e5e38607f155292
_michal <sz-m@users.noreply.github.com>
2016-07-09 12:32:35 +0200

    client/main fix

--- client/main.jsx
+++ client/main.jsx
@@ -2,8 +2,22 @@ import {Meteor} from "meteor/meteor";
 import React from 'react';
 import {render} from 'react-dom';
 
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+
 import App from '/imports/ui/App.jsx';
 
+
+const RenderApp = () => (
+	<MuiThemeProvider muiTheme={getMuiTheme()}>
+		<App />
+	</MuiThemeProvider>
+);
+
+
 Meteor.startup(() => {
-  render(<App />, document.getElementById('app'));
+	render(
+	  <RenderApp />,
+	  document.getElementById('app')
+	);
 });
