403cbaa7c4436fc7f7b8cc84da798dba73627997
Jan <jan.owsiany@vazco.eu>
2016-07-09 12:58:38 +0200

    v0.0.0.1

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,6 +1,8 @@
 // main
-import React, {PropTypes} from 'react';
-import {Meteor}           from 'meteor/meteor';
+import React, { Component, PropTypes } from 'react';
+import ReactDOM                        from 'react-dom';
+import {Meteor}                        from 'meteor/meteor';
+import { createContainer }             from 'meteor/react-meteor-data';
 
 // material-ui
 import injectTapEventPlugin from 'react-tap-event-plugin';
@@ -11,16 +13,65 @@ import {Tabs, Tab}  from 'material-ui/Tabs';
 // ?
 injectTapEventPlugin();
 
-class App extends React.Component {
+//custom
+import { Events } from '../api/Events.js';
+import Event from './Event.jsx';
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+        };
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+        Events.insert({
+          text,
+          createdAt: new Date(),
+        });
+
+        ReactDOM.findDOMNode(this.refs.textInput).value = '';
+    }
+
+    renderEvents() {
+        return (this.props.events || []).map((event) => (
+          <Event key={event._id} event={event} />
+        ));
+    }
+
     render() {
-      return(
-        <Tabs>
-          <Tab icon={<FontIcon className="material-icons">assignment</FontIcon>} 
-               label="EVENTS LIST">
-          </Tab>
-        </Tabs>
-      );
+        return (
+            <div className="container">
+                <header>
+                      <form className="" onSubmit={this.handleSubmit.bind(this)} >
+                            <input
+                              type="text"
+                              ref="textInput"
+                              placeholder="Type to add new event"
+                            />
+                      </form>
+                </header>
+
+                <ul>
+                    {this.renderEvents()}
+                </ul>
+            </div>
+        );
     }
 }
 
-export default App;
+App.propTypes = {
+    events: PropTypes.array.isRequired
+};
+
+export default createContainer(() => {
+    return {
+        events: Events.find({}, { sort: { createdAt: -1 } }).fetch()
+    };
+}, App);
